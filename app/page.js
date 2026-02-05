2b03d48a9da83cc7d876a50653842fd39dede4e9
diogosouusa444-cpu <diogosouusa444@gmail.com>
2026-02-04 23:07:41 -0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Topbar from "@/app/components/Topbar";
+import Lobby from "@/app/ui/Lobby";
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <Lobby />
+      <div className="footer">DG77 • Demo apenas (sem dinheiro real)</div>
+    </>
+  );
+}
