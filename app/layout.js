fb2a3d7b1279aa659315a577ba34f06974c0dc18
diogosouusa444-cpu <diogosouusa444@gmail.com>
2026-02-04 23:43:20 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+"use client";
+
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <div className="wrap">{children}</div>
+      </body>
+    </html>
+  );
+}
 
